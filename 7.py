b0861a81680ae98955f7c7c07800ce46fdb71bfe
hoan738888-cpu <hoan738888@gmail.com>
2026-01-16 13:28:52 +0700

    Add files via upload
    final result for submit

---
+++ 7.py
@@ -0,0 +1,5 @@
+import random
+code_3 = [random.randint(0, 9) for _ in range(3)]
+code_4 = [random.randint(1, 6) for _ in range(4)]
+print("3-digit lock code:", code_3)
+print("4-digit lock code:", code_4)
