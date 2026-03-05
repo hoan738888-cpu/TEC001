5e5317f0bce4465de61fc04bd9dbfe440dada789
hoan738888-cpu <hoan738888@gmail.com>
2026-03-05 23:44:38 +0700

    Update 2.py

--- AS5/2.py
+++ AS5/2.py
@@ -1,8 +1,6 @@
 import re
-def is_valid_hex_color(color: str) -> bool:
-    return bool(re.match(r'^#[0-9A-Fa-f]{6}$', color))
-color  = input('Enter a color: ')
-if is_valid_hex_color(color)    :
-    print("TRUE")
-else:
-    print("FALSE")
+def is_valid_hex_color(color: str) -> str:
+    match = re.match(r'^#[0-9A-Fa-f]{6}$', color)
+    return 'TRUE' if match else 'FALSE'
+color = input('Enter a color: ')
+print(is_valid_hex_color(color))
