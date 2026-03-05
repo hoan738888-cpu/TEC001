0c4f901c6ced4ec5521daaf0967f5f12929dfefd
hoan738888-cpu <hoan738888@gmail.com>
2026-03-05 23:44:16 +0700

    Update 1.py

--- AS5/1.py
+++ AS5/1.py
@@ -1,10 +1,6 @@
 import re
-
-def is_valid_course_code(code: str) -> bool:
-    return bool(re.match(r'^[A-Z]{2,3}\d{3}$', code))
+def is_valid_course_code(code: str) -> str:
+    match = re.match(r'^[A-Z]{2,3}\d{3}$', code)
+    return 'TRUE' if match else 'FALSE'
 pattern = input('Enter a course code: ')
-if is_valid_course_code(pattern):
-    print('TRUE')
-else:
-    print('FALSE')
-
+print(is_valid_course_code(pattern))
